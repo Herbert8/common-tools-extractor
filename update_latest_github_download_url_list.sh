d136715bc34b423c9d96eeb6a6cda9749207049e
重劍無鋒 <tecpoirot@foxmail.com>
2025-03-03 14:57:51 +0800

    feat: 增加 aarch64 平台的支持，重构脚本结构和调用关系。

--- update_latest_github_download_url_list.sh
+++ update_latest_github_download_url_list.sh
@@ -3,9 +3,21 @@
 THIS_SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 readonly THIS_SCRIPT_DIR
 
-SOFTWARE_LIST=$THIS_SCRIPT_DIR/standalone_tool_list.sh
-readonly SOFTWARE_LIST
+# shellcheck source=/dev/null
+source "$THIS_SCRIPT_DIR/list_processor_lib.sh"
 
-[[ -r "$SOFTWARE_LIST" ]] && source "$SOFTWARE_LIST"
+parse_soft_list() {
+    local src_list_file=$1
+    local dest_list_file=$2
 
-get_all_github_software_download_url "$THIS_SCRIPT_DIR/data/github_download_url_list_latest.txt"
+    clear_software_array_list
+
+    # shellcheck source=/dev/null
+    source "$src_list_file"
+
+    get_all_github_software_download_url "$dest_list_file"
+}
+
+
+parse_soft_list "$THIS_SCRIPT_DIR/standalone_tool_list_x86_64.env" "$THIS_SCRIPT_DIR/data/github_download_url_list_x86_64_latest.txt"
+parse_soft_list "$THIS_SCRIPT_DIR/standalone_tool_list_aarch64.env" "$THIS_SCRIPT_DIR/data/github_download_url_list_aarch64_latest.txt"
